c3cb61ffae2d53d0ea2c9d9ca9feca87d20d8888
hulu00810 <143834762+hulu00810@users.noreply.github.com>
2025-04-26 11:46:46 +0700

    update sched

--- src/sched.c
+++ src/sched.c
@@ -81,7 +81,6 @@ void put_mlq_proc(struct pcb_t * proc) {
 void add_mlq_proc(struct pcb_t * proc) {
 	pthread_mutex_lock(&queue_lock);
 	enqueue(&mlq_ready_queue[proc->prio], proc);
-	dequeue_running(&running_list, proc);
 	pthread_mutex_unlock(&queue_lock);	
 }
 
